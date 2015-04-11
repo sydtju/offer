cd79811b26b70926f3b65c7fede120b43bb8ebef
yadongshao <348829719@qq.com>
2015-04-11 16:54:50 +0800

    Update 20150411.cpp

--- 20150411.cpp
+++ 20150411.cpp
@@ -20,3 +20,23 @@ public:
             
 	}
 };
+//一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+class Solution {
+public:
+	int jumpFloorII(int number) {
+		int counter=1;
+        int temp1=1;
+        int temp=0;
+        if(number<0) return -1;
+		if(number==0) return 1;
+        else if(number==1) return 1;
+        else {
+            while(counter<number){
+                temp=2*temp1;
+                temp1=temp;
+                counter++;
+            }
+            return temp1;
+        }
+	}
+};
