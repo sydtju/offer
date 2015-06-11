4e0269bd422c8f852b3076666f8a3ca34856c894
yadongshao <348829719@qq.com>
2015-06-11 22:59:11 +0800

    Create 链表的环的入口结点.cpp

---
+++ second/链表的环的入口结点.cpp
@@ -0,0 +1,33 @@
+//一个链表中包含环，请找出该链表的环的入口结点。
+//用map是n*log(n)的复杂度  用hash_map是n的复杂度
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
+*/
+class Solution {
+public:
+	ListNode* EntryNodeOfLoop(ListNode* pHead)
+	{
+		map<ListNode*,int> hmap;
+		ListNode* point=pHead;
+		map<ListNode*,int>::iterator it;
+		while (point)
+		{
+			it=hmap.find(point);
+			if (it!=hmap.end())
+			{
+				return point;
+			}
+			else{
+				hmap.insert(pair<ListNode*,int>(point,point->val));
+			}
+			point=point->next;
+		}
+		return NULL;
+	}
+};
