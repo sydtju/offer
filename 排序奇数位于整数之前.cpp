6ed978c04213c38f2dc93195c5a1e4bd6980463c
yadongshao <348829719@qq.com>
2015-07-14 21:02:39 +0800

    Update 排序奇数位于整数之前.cpp

--- 排序奇数位于整数之前.cpp
+++ 排序奇数位于整数之前.cpp
@@ -1,6 +1,6 @@
 //输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
 //所有的偶数位于位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
-//
+//这个不是最简单的算法  简单算法应该是partition
 class Solution {
 public:
 	void reOrderArray(vector<int> &array) {
