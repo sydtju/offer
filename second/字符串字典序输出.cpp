d9feeecf2d862dc8035d76b1d49de487ac86ffee
yadongshao <348829719@qq.com>
2015-08-09 13:55:09 +0800

    Create 字符串字典序输出.cpp

---
+++ second/字符串字典序输出.cpp
@@ -0,0 +1,19 @@
+//字符串按字典序输出
+//题目描述
+///*输入一个字符串,按字典序打印出该字符串中字符的所有排列。例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。 
+//输入描述:
+//输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+//*/
+class Solution {
+public:
+    vector<string> Permutation(string str) {
+        vector<string> result;
+        if(str.length()==0) return result;
+        sort(str.begin(),str.end());
+        result.push_back(str);
+        while(next_permutation(str.begin(),str.end())){
+              result.push_back(str);
+        }
+        return result;
+    }
+};
