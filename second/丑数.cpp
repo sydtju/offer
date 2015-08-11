7877f0a1cfeb5ae4f0320869288d1ee9de4538fb
yadongshao <348829719@qq.com>
2015-08-11 12:06:29 +0800

    Update 丑数.cpp

--- second/丑数.cpp
+++ second/丑数.cpp
@@ -18,3 +18,29 @@ public:
         return number;
     }
 };
+//这种方法  可以减少时间复杂度  可以通过OJ  可以使用打表的方式  然后使用进行查找 
+class Solution {
+public:
+int min(int a,int b,int c){
+	int min_temp=a>b?b:a;
+	return  min_temp>c?c:min_temp;
+}
+int GetUglyNumber_Solution(int index){
+	int index_std=0,index2=0,index3=0,index5=0;
+	if (index<=0) return 0;
+	int value=1;
+	int *array=new int[index];
+	array[0]=1;
+	while (index_std<index-1)
+	{
+		value=min(array[index2]*2,array[index3]*3,array[index5]*5);
+		if (array[index2]*2==value) index2++;
+		if (array[index3]*3==value) index3++;
+		if (array[index5]*5==value) index5++;
+		array[++index_std]=value;
+	}
+	int result=array[index_std];
+	delete[] array;
+	return result;
+}
+};
