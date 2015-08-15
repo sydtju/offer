5534bd628daa58293de8fefaac42519960ffff5f
yadongshao <348829719@qq.com>
2015-08-15 09:53:43 +0800

    Create 扑克牌顺序.cpp

---
+++ second/扑克牌顺序.cpp
@@ -0,0 +1,30 @@
+//LL今天心情特别好,因为他去买了一副扑克牌,发现里面居然有2个大王,2个小王(一副牌原本是54张^_^)...
+//他随机从中抽出了5张牌,想测测自己的手气,看看能不能抽到顺子,如果抽到的话,他决定去买体育彩票,嘿嘿！！
+//“红心A,黑桃3,小王,大王,方片5”,“Oh My God!”不是顺子.....LL不高兴了,他想了想,决定大\小 王可以看成任何数字,
+//并且A看作1,J为11,Q为12,K为13。上面的5张牌就可以变成“1,2,3,4,5”(大小王分别看作2和4),“So Lucky!”。LL决定去买体育彩票啦。 
+//现在,要求你使用这幅牌模拟上面的过程,然后告诉我们LL的运气如何。为了方便起见,你可以认为大小王是0。
+
+class Solution {
+public:
+    bool IsContinuous( vector<int> numbers ) {
+        vector<int>::iterator begin,end;
+        int length=(int)numbers.size();
+        if(!length) return false;
+        begin=numbers.begin();
+        end=numbers.end();
+        sort(begin,end);
+        int i0=0;
+		while(!numbers[i0])i0++;
+        if(i0>length-1) return false;
+        int std=numbers[i0];
+        int j=i0+1;
+        while(j<length){
+            int tap=numbers[j]-std;
+            if(tap>1) i0=i0-tap+1;
+           	if(tap<1) return false;
+            std=numbers[j++];
+            if(i0<0) return false;
+        }
+        return true;
+    }
+};
